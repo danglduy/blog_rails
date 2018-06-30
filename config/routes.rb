5c83b1efe2e3664c1da905ac50f6ea5e377e81ce
zldang <38782010+zldang@users.noreply.github.com>
2018-06-30 16:28:34 +0000

    logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
   root 'master#home'
+  
   get 'help', to: 'master#help'
   get 'about', to: 'master#about'
   get 'contact', to: 'master#contact'
+  
   get 'signup', to: 'users#new'
   post 'signup', to: 'users#create'
+
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
   resources :users
 end
