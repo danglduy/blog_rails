eae1c2961ad4a3a60770f7793344ac8f0173697e
zldang <38782010+zldang@users.noreply.github.com>
2018-07-01 08:23:03 +0000

    basic menu

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,7 @@ User.create!(name:  "123",
              password:              "123123",
              password_confirmation: "123123",
              admin: true,
-             activated: true
+             activated: true,
              activated_at: Time.zone.now)
 
 99.times do |n|
@@ -14,6 +14,6 @@ User.create!(name:  "123",
                email: email,
                password:              password,
                password_confirmation: password,
-               activated: true
+               activated: true,
                activated_at: Time.zone.now)
 end
