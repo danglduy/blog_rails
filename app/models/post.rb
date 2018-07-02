3d21143602e3afeaa599682b5a161c3aad02d84a
zldang <38782010+zldang@users.noreply.github.com>
2018-07-02 05:05:34 +0000

    reformat

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ApplicationRecord
   has_many :comments, dependent: :destroy
   belongs_to :user
+
   default_scope -> { order(created_at: :desc) }
   validates :user_id, presence: true
   validates :title, presence: true
