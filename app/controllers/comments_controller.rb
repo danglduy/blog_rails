728ebcbb9e82effb079362adc14fae70774cd780
zldang <38782010+zldang@users.noreply.github.com>
2018-07-02 18:09:09 +0000

    format pages

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,7 @@
 class CommentsController < ApplicationController
   before_action :logged_in_user
   before_action :correct_user, only: [:destroy]
+
   def create
     post = Post.find_by(id: params[:post_id])
     @comment = post.comments.build(comment_params)
@@ -18,7 +19,6 @@ class CommentsController < ApplicationController
         redirect_to post_path(@comment.commentable)
     end
   end
-
 
   def destroy
     comment = Comment.find_by(id: params[:id])
@@ -42,6 +42,4 @@ class CommentsController < ApplicationController
   def comment_params
     params.require(:comment).permit(:content).merge(user_id: current_user.id)
   end
-
-
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find_by(id: params[:id])
-    @posts = @user.posts.paginate(page: params[:page], per_page: 20)
+    @posts = @user.posts.paginate(page: params[:page], per_page: 10)
   end
 
   def new
