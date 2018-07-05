0e6ff139e9dc5457d733bd671bc8df84dd1ba13c
zldang <38782010+zldang@users.noreply.github.com>
2018-07-05 02:28:04 +0000

    fix user-comments relation, destroy.js

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,13 +2,12 @@ class UsersController < ApplicationController
   before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
   before_action :correct_user,   only: [:edit, :update]
   before_action :admin_user,     only: [:destroy]
-
+  before_action :find_user,      only: [:edit, :update, :show, :destroy, :following, :followers]
   def index
     @users = User.paginate(page: params[:page], per_page: 10)
   end
 
   def show
-    @user = User.find_by(id: params[:id])
     @posts = @user.posts.paginate(page: params[:page], per_page: 10)
     @postpage = true
   end
@@ -29,11 +28,9 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find_by(id: params[:id])
   end
 
   def update
-    @user = User.find_by(id: params[:id])
     if @user.update_attributes(user_params)
       flash[:success] = "Profile updated!"
       redirect_to @user
@@ -43,21 +40,25 @@ class UsersController < ApplicationController
   end
 
   def destroy
-    User.find(params[:id]).destroy
-    flash[:success] = "User deleted"
-    redirect_to users_url
+    @id = @user.id
+    @user.destroy
+    respond_to do |format|
+      format.html {
+        flash[:success] = "User deleted"
+        redirect_to users_url
+      }
+      format.js
+    end
   end
 
   def following
     @title = "Following"
-    @user  = User.find(params[:id])
     @users = @user.following.paginate(page: params[:page], per_page: 10)
     render 'show_follow'
   end
 
   def followers
     @title = "Followers"
-    @user  = User.find(params[:id])
     @users = @user.followers.paginate(page: params[:page], per_page: 10)
     render 'show_follow'
   end
@@ -76,6 +77,10 @@ class UsersController < ApplicationController
 
   def admin_user
     redirect_to(root_url) unless current_user.admin?
+  end
+
+  def find_user
+    @user = User.find_by(id: params[:id])
   end
 
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,7 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :commentable, polymorphic: true
-  has_many :comments, as: :commentable
+  has_many :comments, as: :commentable, dependent: :destroy
 
   default_scope -> { order(created_at: :desc)  }
   validates :user_id, presence: true
