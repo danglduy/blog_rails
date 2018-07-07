bf5831b34e5125da30efed698ee8eda330aba0ec
zldang <38782010+zldang@users.noreply.github.com>
2018-07-07 16:11:09 +0000

    non-login home page

--- app/controllers/master_controller.rb
+++ app/controllers/master_controller.rb
@@ -4,6 +4,9 @@ class MasterController < ApplicationController
     if logged_in?
       @posts = current_user.feed.paginate(page: params[:page], per_page: 10)
       render 'feed'
+    else
+      @posts = Post.paginate(page: params[:page], per_page: 10)
+      render 'posts/index'
     end
   end
   def help
