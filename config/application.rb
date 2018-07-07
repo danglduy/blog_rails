94eb60275fba749fa2c0df752234ab8f1ae50628
zldang <38782010+zldang@users.noreply.github.com>
2018-07-07 07:20:03 +0000

    change to localhost

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,7 @@ module DemoBlog
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
     config.action_view.embed_authenticity_token_in_remote_forms = true
-    config.action_mailer.default_url_options = { host: "139.162.54.118", port:3000 }
+    config.action_mailer.default_url_options = { host: "localhost", port:3000 }
     config.action_mailer.perform_deliveries = true
     config.action_mailer.raise_delivery_errors = true
     config.action_mailer.delivery_method = :smtp
