01ca50d6ed519046e0de9f93f30715dab27bd5ba
danglduy <38782010+danglduy@users.noreply.github.com>
2018-09-17 07:01:37 +0000

    fix devise methods

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,5 @@
 class CommentsController < ApplicationController
-  before_action :logged_in_user
+  before_action :authenticate_user!
   before_action :correct_user, only: [:destroy]
   before_action :find_commentable
   before_action :find_comment, only: [:destroy]
