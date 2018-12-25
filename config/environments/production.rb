eeeb3e239d31a619126778bf5443d46a31a13837
danglduy <38782010+danglduy@users.noreply.github.com>
2018-12-25 15:51:23 +0700

    production mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,6 +65,21 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  # Set action mailer default_url
+  config.action_mailer.default_url_options = {host: "localhost", port: 3000}
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              ENV['email_address'],
+    port:                 ENV['email_port'],
+    domain:               ENV['email_domain'],
+    user_name:            ENV['email_username'],
+    password:             ENV['email_password'],
+    authentication:       'plain',
+    enable_starttls_auto: true
+  }
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
