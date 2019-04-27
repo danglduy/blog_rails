1e9b4825597c7c1edf1f75f9094a00143f2a7640
danglduy <38782010+danglduy@users.noreply.github.com>
2019-04-28 01:25:59 +0700

    fix production email config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,6 +28,21 @@ Rails.application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
+  # Set action mailer default_url
+  config.action_mailer.default_url_options = {host: "localhost", port: 3000}
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              ENV['email_address'],
+    port:                 ENV['email_port'],
+    domain:               ENV['email_domain'],
+    user_name:            ENV['email_username'],
+    password:             ENV['email_password'],
+    authentication:       'plain',
+    enable_starttls_auto: true
+  }
+
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
 
