a8a6efd01f89e8da716aadb9b73faa5da948408c
danglduy <38782010+danglduy@users.noreply.github.com>
2019-04-28 20:09:58 +0700

    migrate asset-pipelines to webpacker

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,16 @@
 const { environment } = require('@rails/webpacker')
+const webpack = require('webpack')
+const erb =  require('./loaders/erb')
+
+environment.loaders.prepend('erb', erb)
+environment.plugins.prepend(
+  'Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery',
+    jQuery: 'jquery',
+    jquery: 'jquery',
+    Popper: ['popper.js', 'default'], // for Bootstrap 4
+  })
+)
 
 module.exports = environment
