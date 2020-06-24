ea29d267dbd8f03c4de74f0e56a432c69bae15dc
danglduy <38782010+danglduy@users.noreply.github.com>
2020-06-24 16:40:14 +0700

    fix: expose-loader

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,8 +9,10 @@ require("channels")
 require("trix")
 require("@rails/actiontext")
 require("bootstrap")
-require("expose-loader?$!jquery");
-require("expose-loader?EasyMDE!easymde");
+
+// Expose jQuery & EasyMDE
+import $ from 'expose-loader?exposes[]=$&exposes[]=jQuery!jquery';
+import EasyMDE from 'expose-loader?exposes[]=$&exposes[]=EasyMDE!easymde';
 
 // Custom javascript
 $(document).on('turbolinks:load', function() {
