144b95b2aa94eaeecd0cd16e5abcdb426ab0e086
danglduy <38782010+danglduy@users.noreply.github.com>
2020-06-25 11:59:06 +0700

    fix: expose-loader & devise error message template

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,10 +9,8 @@ require("channels")
 require("trix")
 require("@rails/actiontext")
 require("bootstrap")
-
-// Expose jQuery & EasyMDE
-import $ from 'expose-loader?exposes[]=$&exposes[]=jQuery!jquery';
-import EasyMDE from 'expose-loader?exposes[]=$&exposes[]=EasyMDE!easymde';
+require("expose-loader?$!jquery");
+require("expose-loader?EasyMDE!easymde");
 
 // Custom javascript
 $(document).on('turbolinks:load', function() {
